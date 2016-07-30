d9d55108757e83dcc5f9157911fe848a63d7cb58
Romain Goffe <romain.goffe@gmail.com>
2016-07-30 11:48:26 +0200

    [statiques] add souvenirs

---
+++ data/statiques/003.souvenirs.php
@@ -0,0 +1,5 @@
+<div class="wrapper margin">
+<ul class="actions">
+<li><a href="https://plus.google.com/collection/A0uhcB" class="button icon fa fa-camera">Galerie des albums photos</a></li>
+</ul>
+</div>
